94084fa6a41301ee379938ecad9d7f679ad9eb21
vivekgopalan <vivekgopalan2230@gmail.com>
2024-06-17 22:37:52 +0530

    fixed

--- src/app/components/Kudos/Kudos.js
+++ src/app/components/Kudos/Kudos.js
@@ -11,18 +11,23 @@ export default function Kudos() {
             <div className={styles.kudosImgWrapper}>
                 <div className={styles.kudosImgContainer}>
                     <img className={styles.kudosImg} src="Images/kudos/IMG_20240606_234250.jpg" alt="kudos-image" />
+                    <div className={styles.viewKudosWrapper}>View Kudos</div>
                 </div>
                 <div className={styles.kudosImgContainer}>
                     <img className={styles.kudosImg} src="Images/kudos/IMG_20240606_233445.jpg" alt="kudos-image" />
+                    <div className={styles.viewKudosWrapper}>View Kudos</div>
                 </div>
                 <div className={styles.kudosImgContainer}>
                     <img className={styles.kudosImg} src="Images/kudos/IMG_20240606_234058.jpg" alt="kudos-image" />
+                    <div className={styles.viewKudosWrapper}>View Kudos</div>
                 </div>
                 <div className={styles.kudosImgContainer}>
                     <img className={styles.kudosImg} src="Images/kudos/IMG_20240606_233714.jpg" alt="kudos-image" />
+                    <div className={styles.viewKudosWrapper}>View Kudos</div>
                 </div>
                 <div className={styles.kudosImgContainer}>
                     <img className={styles.kudosImg} src="Images/kudos/IMG_20240606_234455.jpg" alt="kudos-image" />
+                    <div className={styles.viewKudosWrapper}>View Kudos</div>
                 </div>
             </div>
         
