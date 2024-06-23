856069b1b5dd9404aafde8d178275c382025731f
vivekgopalan <vivekgopalan2230@gmail.com>
2024-06-24 03:03:59 +0530

    Fixed

---
+++ src/app/projects/page.js
@@ -0,0 +1,35 @@
+import styles from "./page.module.css";
+import {ProjectCardData} from "./projectData";
+
+export default function Projects() {
+
+    console.log("ProjectCardData",ProjectCardData)
+  return (
+    <>
+        <div className={styles.projectPageWrapper}>
+            {
+                ProjectCardData.map((data,index)=>{
+
+                    return(
+                        <div className={styles.projectPageCardWrap}>
+                            <div className={styles.projectPageCompanyName}>{data.companyName}</div>
+                            {
+                                data.workData.map((data,index)=>{
+                                    return(
+                                        <>
+                                            <div className={styles.projectPageCardTitle}>{data.title}</div>
+                                            <div dangerouslySetInnerHTML={{__html:data.content}} className={styles.projectPageCardContent}></div>
+                                        </>
+                                    )
+                                })
+                            }
+                           
+                        </div>
+                    )
+                })
+            }
+               
+        </div>
+    </>
+  );
+}
---
+++ src/app/projects/projectData.js
@@ -0,0 +1,98 @@
+export const ProjectCardData = [
+    {
+        "companyName":"Valeo Wellbeing Technologies",
+         "workData":[
+            {
+                title:"Website:https://feelvaleo.com",
+                "content":`
+                <p>Fixed caching issues: When we do any new deployment, the latest changes are not reflected.</p>
+                <p>Website setup:</p>
+                <ul>
+                    <li>Set up the environment-specific configurations to pick up the development, stage, and production files separately</li>
+                    <li>Set up route-based code splitting using webpack to optimise page load performance</li>
+                    <li>Added Node server setup for handling backend requests.</li>
+                    <li>Webpack hot reloading for development purposes.</li>
+                    <li>Implemented Redux setup to maintain the state.</li>
+                </ul>
+                <p>Deploy a website using PM2 and set up a reverse proxy on an Nginx server.</p>
+                <p>Website Performance and SEO :</p>
+                <ul>
+                    <li>Implemented server side rendering</li>
+                    <li>Fixed Seo Issues</li>
+                    <li>Added lazy loading for image</li>
+                    <li>Optimised Js bundle and Css bundle</li>
+                </ul>
+                <p>Developed PDF invoice template for sending emails to customers.</p>
+                <p>Payment Integration:</p>
+                <ul>
+                    <li>Apple pay</li>
+                    <li>Checkout</li>
+                    <li>Tamara</li>
+                    <li>Cod</li>
+                </ul>
+                <p>Developed Pages:</p>
+                <ul>
+                    <li>Supplement Page</li>
+                    <li>Supplement Detail Page</li>
+                    <li>Custom Mini Package</li>
+                    <li>Treatment/Solutions Page</li>
+                    <li>Add/on page</li>
+                    <li>Cart Page</li>
+                    <li>Payment Review</li>
+                    <li>Homepage design revamp</li>
+                    <li>Saudi National Day Page</li>
+                </ul>
+                <p>Integrated CleverTap (third-party library) for analytics purposes.</p>
+                <p>Added region-based automatic switching of the country for users.</p>
+                <p>&nbsp;</p>
+                `
+            },
+            {
+                title:"Mobile App",
+                content:`<p><span style="white-space:pre-wrap;">Integrated CleverTap (third-party library) for analytics purposes.</span><br><span style="white-space:pre-wrap;">Integrated Adjust (Third-party library) for analytics purposes.</span><br><span style="white-space:pre-wrap;">Fixed push notifications issues in Android and iOS.</span></p>
+                <p><span style="white-space:pre-wrap;">Payment Integration:</span></p>
+                <ul>
+                    <li><span style="white-space:pre-wrap;">Checkout</span></li>
+                    <li><span style="white-space:pre-wrap;">Paytabs 2.0 upgraded</span></li>
+                    <li><span style="white-space:pre-wrap;">Apple pay</span></li>
+                    <li><span style="white-space:pre-wrap;">Tamara integration</span></li>
+                    <li><span style="white-space:pre-wrap;">Cod</span></li>
+                </ul>
+                <p><span style="white-space:pre-wrap;">Fixed re-render issues on some of the screens.</span></p>
+                <p><span style="white-space:pre-wrap;">Set up the Play Store and App Store rating prompt after the user receives coach feedback.</span></p>
+                <p><span style="white-space:pre-wrap;">Developed Some of the screens.</span></p>
+                <ul>
+                    <li><span style="white-space:pre-wrap;">Custom mini package flow</span></li>
+                    <li><span style="white-space:pre-wrap;">Added Coach suggestion Flow</span></li>
+                    <li><span style="white-space:pre-wrap;">Live result flow and attention required flow&nbsp;</span></li>
+                    <li><span style="white-space:pre-wrap;">Health hub flow</span></li>
+                </ul>`
+            },
+            {
+                title:"Admin Panel",
+                content:`<p><span style="white-space:pre-wrap;">Fixed caching issues: When we do any new deployment, the latest changes are not reflected.</span></p>
+                <p><span style="white-space:pre-wrap;">I was helping set up web push notifications for the nurse panel using Firebase.</span></p>`
+            },
+            {
+                title:"kuwa:https://bh.getkuwa.com",
+                content:`<p><span style="white-space:pre-wrap;">Setting up the website using Next.js and React, including the deployment setup  Developed the complete flow of the Kuwa website(sign-in,signup,address page,cart page,payment page, my orders, order details and product page)  Integrated Clevertap events and Google analytics</span></p>`
+            },
+            {
+                title:"kuwa Admin",
+                content:`<p><span style="white-space:pre-wrap;">Developed on Create order flow and Create Product page</span></p>`
+            }
+         ]
+    },
+    {
+        "companyName":"Myntra Designs PVT Ltd",
+        "workData":[
+            {
+                title:"d",
+                content:``
+            }
+        ]
+
+    }
+]
+
+
